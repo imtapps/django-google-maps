df1a4eb6e3d680addb8e29665f168917a5eea6f7
Matthew J Morrison <matt.morrison@theimtgroup.com>
2011-02-25 09:01:58 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+    name="django-google-maps",
+    version="0.4",
+    description="Django Google Maps",
+    author="Aaron Madison",
+    packages=('django_google_maps',),
+    package_dir={'': 'src'},
+    install_requires = (
+
+    ),
+)
